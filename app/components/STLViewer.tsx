4bf004e67686aea84da48a92e64097a722b9ca14
ksk10052002 <ksk10052002@gmail.com>
2026-03-08 23:39:10 +0530

    Updated_Preview_Section

--- app/components/STLViewer.tsx
+++ app/components/STLViewer.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { Canvas, useLoader } from "@react-three/fiber";
-import { OrbitControls } from "@react-three/drei";
+import { OrbitControls, Grid } from "@react-three/drei";
 import { STLLoader } from "three-stdlib";
 import { useRef } from "react";
 
@@ -15,9 +15,16 @@ function Model({ url }: { url: string }) {
 
   const box = geometry.boundingBox;
   const size = new THREE.Vector3();
+  const centre = new THREE.Vector3();
+
   box.getSize(size);
+  box?.getCenter(centre);
 
   const scale = 60 / Math.max(size.x, size.y, size.z);
+  
+  geometry.translate(-centre.x, -box.min.y, -centre.z);
+
+  // geometry.center();
 
   return (
     <mesh geometry={geometry} scale={scale}>
@@ -34,19 +41,44 @@ export default function STLViewer({ fileUrl }: { fileUrl: string }) {
   
   return (
     <div ref = {containerRef} className = "w-full h-full">
-    <Canvas
-      style={{ width: "100%", height: "100%" }}
-      camera={{ position: [0, 0, 120] }}
-    >
-      <ambientLight intensity={0.6} />
-      <directionalLight position={[10, 10, 10]} />
-      <Model url={fileUrl} />
-      <OrbitControls
-         enableZoom={true}
-         enablePan={false}
-         enableRotate={true}
-       />
-    </Canvas>
+  <Canvas
+    style={{ width: "100%", height: "100%" }}
+    camera={{ position: [0, 80, 120], fov: 50 }}
+  >
+
+    <ambientLight intensity={0.8} />
+    <directionalLight position={[50, 50, 50]} intensity={1} />
+
+    {/* Build Plate */}
+    <mesh rotation={[-Math.PI / 2, 0, 0]} position={[0, -1, 0]}>
+      <planeGeometry args={[200, 200]} />
+      <meshStandardMaterial color="#1a1a1a" />
+    </mesh>
+
+    {/* Grid */}
+    <Grid
+      args={[200, 200]}
+      cellSize={10}
+      cellThickness={0.5}
+      cellColor="#6f6f6f"
+      sectionSize={50}
+      sectionThickness={1}
+      sectionColor="#9d4b4b"
+      fadeDistance={300}
+      fadeStrength={1}
+      followCamera={false}
+      infiniteGrid={false}
+    />
+
+    <Model url={fileUrl} />
+
+    <OrbitControls
+      enableZoom
+      enablePan
+      enableRotate
+    />
+
+  </Canvas>
     </div>
   );
 }
